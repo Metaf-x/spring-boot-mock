b20b8daa1abc5508c66d1188adecb487d68ebbcb
Olga Riabenko <72602961+Metaf-x@users.noreply.github.com>
2025-08-14 14:13:01 +0300

    заглушка из Json файла

--- src/main/java/org/spring/mock/Controller/MainController.java
+++ src/main/java/org/spring/mock/Controller/MainController.java
@@ -1,15 +1,34 @@
 package org.spring.mock.Controller;
 
-import org.apache.catalina.loader.ResourceEntry;
+import jakarta.annotation.PostConstruct;
 import org.spring.mock.Model.Person;
 import org.spring.mock.Model.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/v1")
 public class MainController {
+
+    private String answerJson;
+
+    @PostConstruct
+    public void init() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        try(BufferedReader br = new BufferedReader(new FileReader("answer.json"))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+        }
+        answerJson = sb.toString();
+    }
+
     @GetMapping("/getMapping")
     public String getMapping() {
         return "get mapping - Hello";
@@ -27,6 +46,11 @@ public class MainController {
         student.setFullname(person.getName() + " " + person.getSurname());
         student.setYearGraduated(person.getAge() + 21);
         return student;
+    }
+
+    @PostMapping("/postPersonJsonReader")
+    public String changePersonJsonReader(@RequestBody Person person) {
+        return String.format(answerJson, person.getName(), person.getSurname(), person.getAge());
     }
 
     @GetMapping("/getWithParams")
